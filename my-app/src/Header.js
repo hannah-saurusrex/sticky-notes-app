67c3373b9c6b69195cbb61c2faf6ea1941d03e15
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-24 13:53:43 -0700

    added interactivity to addNote function by using props to pass reference to the addNote event, and updated state wiht placeholder data

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 var Header = props => (
-    <header>
+    <header className="app-header">
         <h1 className="app-header__title">Super Sticky Notes</h1>
         <aside className="app-header__controls">
-        <button className="add-new">+ New Note</button>
+        <button 
+            className="add-new"
+            onClick={props.addNote}>
+                + New Note
+        </button>
         <input 
             className="search" 
             type="text" 
