bf8bb0087eb252e2754ec82718b76dce671c7adf
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-23 17:50:09 -0700

    added comments in component files to describe what is happening

--- my-app/src/Note.js
+++ my-app/src/Note.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// single note. using props to add the description
 
 var Note = props => (
     <li className="note">
--- my-app/src/NotesList.js
+++ my-app/src/NotesList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Note from './Note';
+// list of all notes, importing single note
+// using props to render the note elements, and filter through search
 
 const keepSearchMatches = note => note.doesMatchSearch;
 const renderNote = note => <Note note={note} key={note.id} />;
